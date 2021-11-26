369eda8fccff3971c8fdc280501df0f53a867f00
Alessio G. Baroni <alessiogiovanni.baroni@gmail.com>
2021-11-26 17:47:28 +0100

    Skeleton of a Spring Boot application

---
+++ fuse/test1/src/main/java/io.github.agbaroni/fuse/Main.java
@@ -0,0 +1,9 @@
+package io.github.agbaroni.fuse;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String... args) {
+    }
+}
